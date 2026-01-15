40a6cef943e1f448796f5f40f6f6a30d6b2fa544
Ali Efe Deniz <dnz.ali.efe@gmail.com>
2026-01-15 16:40:28 +0300

    A card structure was created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Program from './components/Program/Program';
 
 function App() {
     return (
         <div className="App">
             <Hero />
+            <Program />
         </div>
     );
 }

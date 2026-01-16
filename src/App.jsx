c81b18b0d40b861dc1cd3b218513d1fa8cb2bf94
Ali Efe Deniz <dnz.ali.efe@gmail.com>
2026-01-16 10:44:21 +0300

    Customer Reviews section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Program from './components/Program/Program';
 import Reasons from './components/Reasons/Reasons';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Program />
             <Reasons />
             <Plans />
+            <Testimonials />
         </div>
     );
 }
